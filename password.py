577763a0fa9613aa9a5454d71e05ef28fcedc03a
Micah <micahkimathi@gmail.com>
2019-05-10 16:59:03 +0300

    Passed the test for finding a password

--- password.py
+++ password.py
@@ -19,6 +19,20 @@ class Password:
         
 
         Password.password_list.remove(self)
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for password in cls.password_list:
+            if password.password == number:
+                return password
 
     def __init__(self,first_name,last_name,password):
 
